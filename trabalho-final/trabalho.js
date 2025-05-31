2f210c2700c989c73c41577515dd17e0792e31d0
VanessaSayuri <sayurivanessa@gmail.com>
2025-05-31 13:45:44 -0300

    organizing trabalho.js file

--- trabalho-final/trabalho.js
+++ trabalho-final/trabalho.js
@@ -1,11 +1,14 @@
 
+//TESTE: QUANDO informar um nome para o Pet, DEVE ser impresso na tag com letras maiúsculas'
+
 function geradorDeTagsDeIdentificacao(nome) {
     return nome.toUpperCase()
 }
 
+//TESTE: QUANDO a idade = 1 + porte M, DEVE ser permitida a adoção
 
 function verificarSePodeSerAdotado(idade, porte){
-  if (idade >=1 && porte == 'M')  {
+  if (idade >=1 && porte === 'M')  {
     return true
   }
   else{
@@ -13,16 +16,15 @@ function verificarSePodeSerAdotado(idade, porte){
   }
 }
 
+//TESTE: QUANDO o peso = 14.5, DEVE ser retornado 4350 gramas para o consumo diário
+
 function calcularConsumoDeRacao(nome, idade, peso){
   let gramasPorDia = peso * 300
   return gramasPorDia
 }
 
 
-
-
-
-
+//TESTE: QUANDO o porte = pequeno, DEVE ser retornada a atividade adequada
 
 function decidirTipoDeAtividadePorPorte(porte) {
 let atividade
@@ -30,7 +32,7 @@ let atividade
 switch (porte) {
     case 'pequeno':
         atividade = `brincar dentro de casa`
-        break;
+    break;
     case 'médio':
         atividade = `caminhada no quarteirao`
     break;
@@ -46,19 +48,20 @@ switch (porte) {
     
 }
 
+//TESTE: QUANDO buscar dado de exemplo, DEVE retornar um valor de forma assíncrona
 
-// async function buscarDadoAsync() {
-//   return 'Pipoca';
-// }
-
-
-const dados = ['Biscoito', 'Pipoca', 'Chocolate'];
+// Criando um array com alguns dados
+const dados = ['Mimosa', 'Pipoca', 'Pituco'];
 
 async function buscarDadoAsync() {
-  const resultado = dados.find(item => item === 'Pipoca');
-  return resultado;
-}
 
+  for (let i = 0; i < dados.length; i++) {
+    if (dados[i] === 'Pipoca') {
+      // Quando encontrar, retorna o valor
+      return dados[i];
+    }
+  }
+}
 
 
 
