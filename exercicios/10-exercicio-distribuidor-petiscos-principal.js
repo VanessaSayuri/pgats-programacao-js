fffae90725ac016c0acd67bd7fa73e36b8f42f1b
VanessaSayuri <sayurivanessa@gmail.com>
2025-05-31 12:07:12 -0300

    add modularization exercises

---
+++ exercicios/10-exercicio-distribuidor-petiscos-principal.js
@@ -0,0 +1,7 @@
+import { CATS, DOGS } from './10-exercicio-distribuidor-petiscos-dados.js'
+import { entregarPetiscos } from './10-exercicio-distribuidor-petiscos-funcao.js'
+
+entregarPetiscos(DOGS)
+console.log('-----------------------');
+
+entregarPetiscos(CATS)
