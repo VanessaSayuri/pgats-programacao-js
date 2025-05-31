85d67a9f36c8ed783c7bd43c5ac5a2b69f4f7d33
VanessaSayuri <sayurivanessa@gmail.com>
2025-05-30 22:11:11 -0300

    modularization concepts

---
+++ conceitos/modularizacao-export.js
@@ -0,0 +1,38 @@
+/**
+ 
+Sintaxe CommonJs:
+    module.exports = {} / require('')
+
+ESM - Ecmascript Standard Modules
+    export {} / import
+
+
+ */
+
+
+    const BRINQUEDOS = [
+  'Osso',
+  'Peteca',
+  'Chocalho'
+]
+
+function exibirNomePet(paramNomePet = 'valor padrao') {
+
+  if (paramNomePet === undefined) {
+    console.log()
+    return
+  }
+
+  console.log(`O nome do pet é ${paramNomePet}`)
+}
+
+function exibirIdade(paramIdadePet) {
+  console.log(`Idade do Pet é ${paramIdadePet}`)
+}
+
+//  export {} / import 
+export {
+  exibirNomePet,
+  exibirIdade,
+  BRINQUEDOS
+}
---
+++ conceitos/modularizacao-import.js
@@ -0,0 +1,6 @@
+// importar { oq queremos} de 'caminho do arquivo'
+
+import { exibirIdade, exibirNomePet } from "./modularizacao-export.js";
+
+exibirNomePet("Pantera")
+exibirIdade(4)
